8c31647b7a6db67b3cbda82aab2b9c0955612b7d
leo-garcianie <leogarcianie037@gmail.com>
2025-08-20 20:59:44 -0600

    Final

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import analyzeText from "./utils/textAnalizer.js";
+import 'dotenv/config'
 
 const app = express();
 const PORT = process.env.PORT || 5000;
