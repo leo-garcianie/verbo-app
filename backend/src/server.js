fad3c3331d0881d5186219675ac6c2b7cf39b64c
leo-garcianie <leogarcianie037@gmail.com>
2025-08-20 21:27:50 -0600

    Update backend build

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,13 +2,25 @@ import express from "express";
 import cors from "cors";
 import analyzeText from "./utils/textAnalizer.js";
 import 'dotenv/config'
+import path from 'path';
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
 // Middleware
 app.use(cors());
 app.use(express.json());
+
+const FRONTEND_FOLDER = "dist";
+
+app.use(express.static(path.join(__dirname, FRONTEND_FOLDER)));
+
+app.get("*", (req, res, next) => {
+    if (req.path.startsWith("/api")) return next();
+    res.sendFile(path.join(__dirname, FRONTEND_FOLDER, "index.html"));
+});
+
 
 // Routes
 app.post("/api/analyze", (req, res) => {
